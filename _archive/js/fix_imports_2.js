bb19573d18015ffdaaac70469331ce419a1c5fe9
bhavishyathrillo <bhavishya@thrillophilia.com>
2026-07-17 13:11:08 +0530

    chore: Clean up _archive folder as codebase is done

--- _archive/js/fix_imports_2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-['src/features/tl/L1SellerViewPage.tsx', 'src/features/cm/L2SellerViewPage.tsx'].forEach(p => {
-  let content = fs.readFileSync(p, 'utf8');
-  content = content.replace(/from '\.\/SellerViewPage'/g, "from '../seller/SellerViewPage'");
-  content = content.replace(/from '\.\/SellerTimelineModal'/g, "from '../seller/SellerTimelineModal'"); // Just in case
-  fs.writeFileSync(p, content, 'utf8');
-});
--- _archive/python/remove_admin_badge.py
+++
@@ -1,8 +0,0 @@
-with open('components/pages/AdminLTAPage.tsx', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace("{ text: 'Admin only', color: 'blue' },", "")
-
-with open('components/pages/AdminLTAPage.tsx', 'w', encoding='utf-8') as f:
-    f.write(text)
-print("Removed Admin only badge")
